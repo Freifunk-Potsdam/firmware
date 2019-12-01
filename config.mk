4d3d919a14806852a02aa97b469e02e4a79c6abe
Seth0r <github@seth0r.net>
2019-12-01 16:32:45 +0100

    supporting two configs
    one with tunneldigger as uplink
    one with direct uplink

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
+PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=fab672cc7aa9b619f69af4ca8bed94f4b8feff03
 SET_BUILDBOT=env
